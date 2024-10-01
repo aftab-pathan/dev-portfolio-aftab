63cb11c87035a98c55980adf167a0a58d67ebd3d
aftab-pathan <aftabksb@gmail.com>
2024-10-02 02:02:17 +0530

    implement Navbar and Hero section UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,14 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Inter, Calistoga } from 'next/font/google';
+import { twMerge } from "tailwind-merge";
 
+const inter = Inter({ subsets: ["latin"], variable: "--font-sans" })
+const calistoga = Calistoga({
+  subsets: ["latin"],
+  variable: "--font-serif",
+  weight: ["400"]
+})
 export const metadata: Metadata = {
   title: "My Portfolio",
   description: "Created with the help of Frontend Tribe",
@@ -13,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={twMerge("bg-gray-900 text-white antialiased font-sans", inter.variable, calistoga.variable)}>{children}</body>
     </html>
   );
 }
--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -1,3 +1,79 @@
+import Image from "next/image";
+import memojiImage from '@/assets/images/memoji-computer.png'
+import ArrowDown from '@/assets/icons/arrow-down.svg'
+import grainImage from '@/assets/images/grain.jpg'
+import StartIcon from '@/assets/icons/star.svg'
+import SparkelIcon from '@/assets/icons/sparkle.svg'
+import { HeroOrbit } from "@/components/HeroOrbit";
+
+
 export const HeroSection = () => {
-  return <div>Hero Section</div>;
+  return <div className="py-32 md:py-48 lg:py-60 relative z-0 overflow-x-clip">
+    <div className="absolute inset-0 [mask-image:liner-gradient(to_bottom,transparent,black_10%,black_70%,transparent)">
+      {/* Green Grain Image as Background   */}
+      <div className="absolute inset-0 -z-30 opacity-5" style={{ backgroundImage: `url(${grainImage.src})` }}></div>
+      {/* Outer ring render  */}
+      <div className="size-[620px] hero-ring" ></div>
+      <div className="size-[820px] hero-ring" ></div>
+      <div className="size-[1020px] hero-ring" ></div>
+      <div className="size-[1220px] hero-ring" ></div>
+      {/* Orbit Start,Sparkel,Round Rendering  */}
+      <HeroOrbit size={800} rotation={-72}>
+        <StartIcon className="size-28 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={20}>
+        <StartIcon className="size-12 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={590} rotation={89}>
+        <StartIcon className="size-8 text-emerald-300" />
+      </HeroOrbit>
+      <HeroOrbit size={430} rotation={-10}>
+        <SparkelIcon className="size-8 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={440} rotation={79}>
+        <SparkelIcon className="size-5 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={550} rotation={170}>
+        <SparkelIcon className="size-11 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={710} rotation={144}>
+        <SparkelIcon className="size-16 text-emerald-300/20" />
+      </HeroOrbit>
+      <HeroOrbit size={720} rotation={85}>
+        <div className="size-3 rounded-full bg-emerald-300/20"></div>
+      </HeroOrbit>
+      <HeroOrbit size={520} rotation={-40}>
+        <div className="size-3 rounded-full bg-emerald-300/20"></div>
+      </HeroOrbit>
+      <HeroOrbit size={650} rotation={-5}>
+        <div className="size-3 rounded-full bg-emerald-300/20"></div>
+      </HeroOrbit>
+    </div>
+
+    <div className="container">
+      <div className="flex flex-col items-center">
+        <Image src={memojiImage} className="size-[100px]" alt="Boy Peeking from behind laptop" />
+        <div className="bg-gray-950 border border-gray-800 px-4 py-1.5 inline-flex items-center gap-4 rounded-lg">
+          <div className="bg-green-500 size-2.5 rounded-full"></div>
+          <div className="text-sm font-medium">Available for new projects</div>
+        </div>
+      </div>
+      <div className="max-w-lg mx-auto">
+        <h1 className="font-serif text-3xl md:text-5xl text-center mt-8 tracking-wide">Building Exceptional User Experiences</h1>
+        <p className="mt-4 text-center text-white/60 md:text-lg">
+          I specialize in transforming design into functional, high-Performance web application. Let's discuss your next project.
+        </p>
+      </div>
+      <div className="flex flex-col md:flex-row justify-center items-center mt-8 gap-4">
+        <button className="inline-flex items-center gap-2 border border-white/15 px-6 h-12 rounded-xl">
+          <span className="font-semibold">Explore My work</span>
+          <ArrowDown className="size-4" />
+        </button>
+        <button className="inline-flex items-center gap-2 border border-white bg-white text-gray-900 h-12 px-6 rounded-xl">
+          <span>👋</span>
+          <span className="font-semibold">Let's Connect</span>
+        </button>
+      </div>
+    </div>
+  </div>;
 };

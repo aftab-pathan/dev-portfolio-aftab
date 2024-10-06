5f865e140e53a99a08f2a0c0d7c4e7053bef2bf5
aftab-pathan <aftabksb@gmail.com>
2024-10-06 19:52:13 +0530

    Implemntation done for Footer Section

--- src/components/ToolboxItems.tsx
+++ src/components/ToolboxItems.tsx
@@ -4,7 +4,7 @@ import { twMerge } from "tailwind-merge";
 
 export const ToolboxItems = ({ items, className, itemsWraperClassName }: { items: { title: string, iconType: React.ElementType }[], className?: string, itemsWraperClassName?: string }) => {
     return (
-        <div className={twMerge("flex [mask-image:liner-gradient(to_right,transparent,black_10%,black_90%,transparent)]", className)}>
+        <div className={twMerge("flex [mask-image:liner-gradient(circle,#000_10%,transparent_80%)]", className)}>
             <div className={twMerge("flex flex-none py-0.5 gap-6 pr-6", itemsWraperClassName)}>
                 {items.map(item => (
                     <div key={item.title} className="inline-flex items-center gap-4 py-2 px-3  outline outline-2 outline-white/10 rounded-lg">
--- src/sections/Tape.tsx
+++ src/sections/Tape.tsx
@@ -25,7 +25,7 @@ export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip">
       <div className="bg-gradient-to-r from-emerald-300 to-sky-400 -rotate-3 -mx-1">
-        <div className="flex [mask-image:liner-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+        <div className="flex [mask-image:liner-gradient(circle,#000_10%,transparent_80%)]">
           <div className="flex gap-4 flex-none py-3">
             {words.map(word => (
               <div key={word} className="inline-flex gap-4 items-center">
